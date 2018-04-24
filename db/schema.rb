b9491b0dac7ebd29cf2aca023adc9e4f925ed913
Grace Saint <gracesaint_2018@depauw.edu>
2018-04-24 00:06:19 +0000

    Add validations for Users and Products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423022704) do
+ActiveRecord::Schema.define(version: 20180423201450) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.decimal "price", precision: 8, scale: 2
+    t.string "color"
+    t.string "size"
+    t.text "description"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
