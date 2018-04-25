b22bb21f0ead155e5a4ec0a4824c008ec549598b
Grace Saint <gracesaint_2018@depauw.edu>
2018-04-25 02:40:27 +0000

    Updated quantities

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,6 +3,7 @@ class OrderItem < ApplicationRecord
     belongs_to :product
     
     validates :order_id, :product, presence: true
+    validates :quantity, numericality: {greater_than_or_equal_to: 0}
     
     def subtotal
       quantity.to_i * product.price
